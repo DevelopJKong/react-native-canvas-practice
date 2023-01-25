e519a68b22c853a6716f7849f0e9ffa064cbb10e
커피볶는정콩 <61773021+DevelopJKong@users.noreply.github.com>
2023-01-25 16:55:53 +0900

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { NativeTouchEvent, useWindowDimensions, View } from "react-native";
+import Canvas from "react-native-canvas";
+import styled from "styled-components/native";
+
+const Touch = styled.Pressable`
+  width: 100%;
+  height: 100%;
+  background-color: salmon;
+`;
 
 export default function App() {
+  const { width, height } = useWindowDimensions();
+  const [XValue, setXValue] = useState<number>(0);
+  const [YValue, setYValue] = useState<number>(0);
+  const handlePress = (e: { nativeEvent: NativeTouchEvent }) => {
+    const X = e.nativeEvent.pageX;
+    const Y = e.nativeEvent.pageY;
+    setXValue(X);
+    setYValue(Y);
+  };
+
+  const handleCanvas = (canvas: Canvas) => {
+    if (canvas) {
+      const ctx = canvas.getContext("2d");
+      canvas.width = width;
+      canvas.height = height;
+      ctx.lineWidth = 5;
+      ctx.lineCap = "round";
+      ctx.lineTo(XValue, YValue);
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(XValue, YValue);
+    }
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Touch onPress={handlePress}>
+        <Canvas ref={handleCanvas} />
+      </Touch>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
