3168b7a96c2f7eb1c972642eb5942a5c0ec7cda8
커피볶는정콩 <61773021+DevelopJKong@users.noreply.github.com>
2023-01-25 17:49:18 +0900

    Camera 설정완료

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
-import { NativeTouchEvent, useWindowDimensions, View } from "react-native";
-import Canvas from "react-native-canvas";
+import { View } from "react-native";
 import styled from "styled-components/native";
+import CameraPreview from "./CameraPreview";
 
 const Touch = styled.Pressable`
   width: 100%;
@@ -10,34 +9,9 @@ const Touch = styled.Pressable`
 `;
 
 export default function App() {
-  const { width, height } = useWindowDimensions();
-  const [XValue, setXValue] = useState<number>(0);
-  const [YValue, setYValue] = useState<number>(0);
-  const handlePress = (e: { nativeEvent: NativeTouchEvent }) => {
-    const X = e.nativeEvent.pageX;
-    const Y = e.nativeEvent.pageY;
-    setXValue(X);
-    setYValue(Y);
-  };
-
-  const handleCanvas = (canvas: Canvas) => {
-    if (canvas) {
-      const ctx = canvas.getContext("2d");
-      canvas.width = width;
-      canvas.height = height;
-      ctx.lineWidth = 5;
-      ctx.lineCap = "round";
-      ctx.lineTo(XValue, YValue);
-      ctx.stroke();
-      ctx.beginPath();
-      ctx.moveTo(XValue, YValue);
-    }
-  };
   return (
     <View>
-      <Touch onPress={handlePress}>
-        <Canvas ref={handleCanvas} />
-      </Touch>
+      <CameraPreview />
     </View>
   );
 }
---
+++ CameraPreview.tsx
@@ -0,0 +1,147 @@
+import { Camera } from "expo-camera";
+import React, { useState, useRef } from "react";
+import {
+  ImageBackground,
+  NativeTouchEvent,
+  Pressable,
+  Text,
+  useWindowDimensions,
+  View,
+  Dimensions,
+} from "react-native";
+import Canvas from "react-native-canvas";
+import styled from "styled-components/native";
+const { height: currentHeight, width: currentWidth } = Dimensions.get("window");
+const CameraBox = styled(Camera)`
+  width: ${currentWidth}px;
+  height: ${currentHeight * 0.7}px;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const CameraPreview = () => {
+  const camera = useRef<Camera>(null);
+  const [photo, setPhoto] = useState<{ uri: string }>({ uri: "" });
+  const [objectBox, setObjectBox] = useState([
+    {
+      name: "test01",
+      BBox: [75, 140, 150, 110],
+    },
+    {
+      name: "test02",
+      BBox: [75, 160, 100, 100],
+    },
+    {
+      name: "test03",
+      BBox: [185, 10, 280, 100],
+    },
+    {
+      name: "test04",
+      BBox: [10, 410, 100, 80],
+    },
+  ]);
+
+  const handleCanvas = async (canvas: Canvas) => {
+    if (canvas) {
+      let ctx = canvas.getContext("2d");
+
+      // canvas size
+      canvas.width = currentWidth;
+      canvas.height = currentHeight;
+
+      ctx.lineWidth = 10;
+      ctx.strokeStyle = "red";
+
+      // Wall
+      for (let obj of objectBox) {
+        ctx.strokeRect(...(obj.BBox as [number, number, number, number]));
+      }
+    }
+  };
+
+  const onPress = (e: { nativeEvent: NativeTouchEvent }) => {
+    console.log("e.nativeEvent", e.nativeEvent);
+    const X = e.nativeEvent.pageX;
+    const Y = e.nativeEvent.pageY;
+
+    const tttt = objectBox
+      .map((value) => ({
+        minX: value.BBox[0],
+        maxX: value.BBox[0] + value.BBox[2],
+        minY: value.BBox[1],
+        maxY: value.BBox[1] + value.BBox[3],
+      }))
+      .filter((item) => {
+        let check = item.minX <= X && item.maxX >= X && item.minY <= Y && item.maxY >= Y;
+        console.log("check", check);
+
+        return check;
+      });
+
+    console.log("tttt", tttt);
+    console.log("X:", X);
+    console.log("Y:", Y);
+  };
+
+  /**
+   * @title 한국인일 경우 idCard를 찍는 함수
+   * @description 한국인 일 경우 신분증을 찍기 위한 api 입니다 한국인은 주민등록증과 운전면허증을 검증합니다
+   * @returns {Promise<void>}
+   */
+  const retakePicture = async () => {
+    try {
+      const options = { quality: 0.5, base64: true };
+      if (camera.current) {
+        const data = await camera.current.takePictureAsync(options);
+        setPhoto({ uri: data.uri });
+      }
+    } catch (error) {
+      console.log("takePicture:::", error); // ! error log
+    }
+  };
+
+  return (
+    <View
+      style={{
+        position: "relative",
+        flex: 1,
+        width: currentWidth,
+        height: currentHeight,
+      }}
+    >
+      {/* 이미지 백그라운드 */}
+      <ImageBackground
+        source={{ uri: photo && photo.uri }}
+        style={{
+          flex: 1,
+        }}
+      />
+
+      <Pressable style={{ backgroundColor: "red" }} onPress={retakePicture}>
+        <Text>다시 촬영</Text>
+      </Pressable>
+
+      <Pressable style={{ backgroundColor: "blue" }}>
+        <Text>캔버스 그리기 테스트</Text>
+      </Pressable>
+
+      <Canvas
+        style={{
+          width: currentWidth,
+          height: currentHeight,
+          position: "absolute",
+          left: 0,
+          top: 0,
+          zIndex: 1000,
+        }}
+        ref={handleCanvas}
+      />
+      <CameraBox ref={camera}>
+        <Pressable style={{ width: 100, height: 100, backgroundColor: "rgba(0,0,0,0.3)" }} onPress={onPress} />
+      </CameraBox>
+    </View>
+  );
+};
+
+export default CameraPreview;
